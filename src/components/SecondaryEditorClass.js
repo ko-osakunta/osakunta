752145e8f5f479a3c91e30a9a5ce161751552b4d
KeremAtak <kerem.atak@hotmail.com>
2018-12-18 05:06:53 +0200

    Text styling more in-depth, needs work

--- src/components/SecondaryEditorClass.js
+++ src/components/SecondaryEditorClass.js
@@ -9,7 +9,7 @@ import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 
 import Editor, { composeDecorators, createEditorStateWithText } from 'draft-js-plugins-editor';
 
-import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
+import createInlineToolbarPlugin, { Separator } from 'draft-js-inline-toolbar-plugin';
 import createLinkPlugin from 'draft-js-anchor-plugin';
 import createImagePlugin from 'draft-js-image-plugin';
 import createAlignmentPlugin from 'draft-js-alignment-plugin';
@@ -17,10 +17,71 @@ import createFocusPlugin from 'draft-js-focus-plugin';
 import createResizeablePlugin from 'draft-js-resizeable-plugin';
 import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
 
-import { ItalicButton, BoldButton, UnderlineButton } from 'draft-js-buttons';
+import {
+    ItalicButton,
+    BoldButton,
+    UnderlineButton,
+    CodeButton,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton,
+  } from 'draft-js-buttons';
 
 import { stateToHTML } from 'draft-js-export-html';
 import ImageAdd from './ImageAdd';
+
+class HeadlinesPicker extends React.Component {
+    componentDidMount() {
+      setTimeout(() => { window.addEventListener('click', this.onWindowClick); });
+    }
+  
+    componentWillUnmount() {
+      window.removeEventListener('click', this.onWindowClick);
+    }
+  
+    onWindowClick = () =>
+      // Call `onOverrideContent` again with `undefined`
+      // so the toolbar can show its regular content again.
+      this.props.onOverrideContent(undefined);
+  
+    render() {
+      const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+      return (
+        <div>
+          {buttons.map((Button, i) => // eslint-disable-next-line
+            <Button key={i} {...this.props} />
+          )}
+        </div>
+      );
+    }
+  }
+  
+  class HeadlinesButton extends React.Component {
+    // When using a click event inside overridden content, mouse down
+    // events needs to be prevented so the focus stays in the editor
+    // and the toolbar remains visible  onMouseDown = (event) => event.preventDefault()
+    onMouseDown = (event) => event.preventDefault()
+  
+    onClick = () =>
+      // A button can call `onOverrideContent` to replace the content
+      // of the toolbar. This can be useful for displaying sub
+      // menus or requesting additional information from the user.
+      this.props.onOverrideContent(HeadlinesPicker);
+  
+    render() {
+      return (
+        <div onMouseDown={this.onMouseDown} className={'headlineButtonWrapper'}>
+          <button onClick={this.onClick} className={'headlineButton'}>
+            H
+          </button>
+        </div>
+      );
+    }
+  }
 
 const linkPlugin = createLinkPlugin();
 const inlineToolbarPlugin = createInlineToolbarPlugin();
@@ -36,18 +97,18 @@ const decorator = composeDecorators(
     alignmentPlugin.decorator,
     focusPlugin.decorator,
     blockDndPlugin.decorator
-  );
+);
 const imagePlugin = createImagePlugin({ decorator });
 
 
 const { AlignmentTool } = alignmentPlugin;
 
-const plugins = [inlineToolbarPlugin, 
+const plugins = [inlineToolbarPlugin,
     linkPlugin,
     blockDndPlugin,
     focusPlugin,
     alignmentPlugin,
-    resizeablePlugin, 
+    resizeablePlugin,
     imagePlugin];
 
 class SecondaryEditorClass extends React.Component {
@@ -104,7 +165,26 @@ class SecondaryEditorClass extends React.Component {
                         plugins={plugins}
                         ref={(element) => { this.editor = element; }}
                     />
-                <AlignmentTool />
+                    <AlignmentTool />
+                    <InlineToolbar>
+                        {
+                            // may be use React.Fragment instead of div to improve perfomance after React 16
+                            (externalProps) => (
+                                <div>
+                                    <BoldButton {...externalProps} />
+                                    <ItalicButton {...externalProps} />
+                                    <UnderlineButton {...externalProps} />
+                                    <CodeButton {...externalProps} />
+                                    <Separator {...externalProps} />
+                                    <HeadlinesButton {...externalProps} />
+                                    <UnorderedListButton {...externalProps} />
+                                    <OrderedListButton {...externalProps} />
+                                    <BlockquoteButton {...externalProps} />
+                                    <CodeBlockButton {...externalProps} />
+                                </div>
+                            )
+                        }
+                    </InlineToolbar>
                 </div>
 
                 <ImageAdd
