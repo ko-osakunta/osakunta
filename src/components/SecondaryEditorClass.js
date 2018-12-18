21488db5576ec50a6fe2d800567c5b69c595410b
KeremAtak <kerem.atak@hotmail.com>
2018-12-18 04:34:31 +0200

    Adding images works

--- src/components/SecondaryEditorClass.js
+++ src/components/SecondaryEditorClass.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import { convertToRaw, RichUtils } from 'draft-js';
-import Editor,  { createEditorStateWithText } from 'draft-js-plugins-editor';
-import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
-import createLinkPlugin from 'draft-js-anchor-plugin';
-import { ItalicButton, BoldButton, UnderlineButton } from 'draft-js-buttons';
-import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 import { databaseRef } from '../config/firebase'
-
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import 'draft-js-alignment-plugin/lib/plugin.css';
+import 'draft-js-focus-plugin/lib/plugin.css';
+import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+
+import Editor, { composeDecorators, createEditorStateWithText } from 'draft-js-plugins-editor';
+
+import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
+import createLinkPlugin from 'draft-js-anchor-plugin';
+import createImagePlugin from 'draft-js-image-plugin';
+import createAlignmentPlugin from 'draft-js-alignment-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
+import createResizeablePlugin from 'draft-js-resizeable-plugin';
+import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
+
+import { ItalicButton, BoldButton, UnderlineButton } from 'draft-js-buttons';
+
 import { stateToHTML } from 'draft-js-export-html';
-import createStyles from 'draft-js-custom-styles';
+import ImageAdd from './ImageAdd';
 
 const linkPlugin = createLinkPlugin();
 const inlineToolbarPlugin = createInlineToolbarPlugin();
+
 const { InlineToolbar } = inlineToolbarPlugin;
-const plugins = [inlineToolbarPlugin, linkPlugin];
-const text = 'Muokkaa sivun tekstiä..';
+const focusPlugin = createFocusPlugin();
+const resizeablePlugin = createResizeablePlugin();
+const blockDndPlugin = createBlockDndPlugin();
+const alignmentPlugin = createAlignmentPlugin();
 
-const customStyleMap = {
-    MARK: {
-        backgroundColor: 'White',
-        fontStyle: 'italic',
-    },
-};
+const decorator = composeDecorators(
+    resizeablePlugin.decorator,
+    alignmentPlugin.decorator,
+    focusPlugin.decorator,
+    blockDndPlugin.decorator
+  );
+const imagePlugin = createImagePlugin({ decorator });
 
-const { styles, customStyleFn, exporter } = createStyles(['font-size', 'color', 'text-transform'], 'CUSTOM_', customStyleMap);
+
+const { AlignmentTool } = alignmentPlugin;
+
+const plugins = [inlineToolbarPlugin, 
+    linkPlugin,
+    blockDndPlugin,
+    focusPlugin,
+    alignmentPlugin,
+    resizeablePlugin, 
+    imagePlugin];
 
 class SecondaryEditorClass extends React.Component {
-   
+
     constructor() {
         super();
         this.state = {
-            editorState: createEditorStateWithText(text)
+            editorState: createEditorStateWithText('Muokkaa sivun tekstiä..')
         }
     }
 
@@ -49,8 +72,7 @@ class SecondaryEditorClass extends React.Component {
 
         var updates = {}
         const { pageKey } = this.props
-        const inlineStyles = exporter(this.state.editorState);
-        updates['pages/' + pageKey + '/text'] = stateToHTML(contentState, { inlineStyles });
+        updates['pages/' + pageKey + '/text'] = stateToHTML(contentState);
         databaseRef.update(updates);
 
         window.location.reload();
@@ -62,7 +84,7 @@ class SecondaryEditorClass extends React.Component {
     focus = () =>
         this.editor.focus();
 
-        handleKeyCommand = (command) => {
+    handleKeyCommand = (command) => {
         const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
         if (newState) {
             this.onChange(newState);
@@ -72,32 +94,30 @@ class SecondaryEditorClass extends React.Component {
     }
 
     render() {
+        const html = stateToHTML(this.state.editorState.getCurrentContent());
         return (
             <div>
-                <div className="editorclass" onClick={this.focus}>
+                <div className="editor" onClick={this.focus}>
                     <Editor
                         editorState={this.state.editorState}
                         onChange={this.onChange}
                         plugins={plugins}
                         ref={(element) => { this.editor = element; }}
                     />
-                    <InlineToolbar>
-                        {
-                            // may be use React.Fragment instead of div to improve perfomance after React 16
-                            (externalProps) => (
-                                <div>
-                                    <BoldButton {...externalProps} />
-                                    <ItalicButton {...externalProps} />
-                                    <UnderlineButton {...externalProps} />
-                                    <linkPlugin.LinkButton {...externalProps} />
-                                </div>
-                            )
-                        }
-                    </InlineToolbar>
+                <AlignmentTool />
                 </div>
+
+                <ImageAdd
+                    editorState={this.state.editorState}
+                    onChange={this.onChange}
+                    modifier={imagePlugin.addImage}
+                />
                 <button className="btn-primary" onClick={this.postNewPage}>
                     Vaihda teksti!
             </button>
+                <div>
+                    {html}
+                </div>
             </div>
         );
     }
