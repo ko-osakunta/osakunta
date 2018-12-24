03a73209ea4512ac32b19a04b11263d00b48a3ec
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-12-24 11:59:16 +0200

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React from "react";
-import Page from "./components/Page";
+import React from "react"
+import Page from "./components/Page"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import * as actions from "./actions";
-import { connect } from "react-redux";
+import { fetchPages } from "./actions"
+import { connect } from "react-redux"
 
 
 class App extends React.Component {
 
-	componentDidMount() {
-		this.props.fetchPages();
-	}
+    componentDidMount() {
+        this.props.fetchPages()
+    }
+    
+    renderRoutes() {
+        let routes = []
+        const { pages } = this.props
+        if (!(Object.keys(pages).length === 0)) {
+            //fetchPages() returns index path as well when not in index, but indexing it to 0 fixes it
+            const key = Object.keys(pages)[0]
+            routes.push(<Route key={pages[key].path} path={pages[key].path} component={Page} />)
+        }
+        return routes
+    }
 
-	renderRoutes() {
-		var routes = []
-		const { pages } = this.props
-		console.log(pages)
-		if (!(Object.keys(pages).length === 0)) {
-			//fetchPages() returns index path as well when not in index, but indexing it to 0 fixes it
-			var key = Object.keys(pages)[0]
-			routes.push(<Route key={pages[key].path} path={pages[key].path} component={Page} />)
-		}
-		return routes
-	}
-
-	render() {
-		return (
-			<Router>
-				<div>
-					{this.renderRoutes()}
-				</div>
-			</Router>
-		);
-	}
+    render() {
+        return (
+            <Router>
+                <div>
+                    {this.renderRoutes()}
+                </div>
+            </Router>
+        )
+    }
 }
 
-const mapStateToProps = ({ pages }) => {
-	return {
-		pages
-	};
-};
+const mapStateToProps = ({ pages }) => ({ pages })
 
-export default connect(mapStateToProps, actions)(App);
+export default connect(mapStateToProps, {fetchPages})(App)
