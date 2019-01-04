15a1ab3d4988c193ee335257207691146865ad3c
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-01-04 23:39:33 +0200

    and forgot to update the component where subcomponents were moved from

--- src/components/structure/Page.js
+++ src/components/structure/Page.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import Banner from './Banner'
 import PagePicker from './PagePicker'
-import Footer from './Footer'
-import SideNav from './SideNav';
 
 //This page contains all the components in the page
-const Page = () =>
-  <div>
-      <Banner />
-      <SideNav />
-      <PagePicker />
-      <Footer />
-  </div>
+const Page = () => <PagePicker />
 
 export default Page;
