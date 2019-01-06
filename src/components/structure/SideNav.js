6a554bc10f553e2c220b6be494fde88db5d22383
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-01-06 16:22:24 +0200

    made sidenav close when link is clicked

--- src/components/structure/SideNav.js
+++ src/components/structure/SideNav.js
@@ -4,11 +4,6 @@ import { Link } from "react-router-dom"
 import styles from "./SideNav.module.css"
 
 class SideNav extends React.Component {
-
-    componentDidMount() {
-        //this.props.fetchTopNav()
-    }
-
     openNav() {
         this.sidenav.style.width = "250px"
     }
@@ -19,24 +14,24 @@ class SideNav extends React.Component {
 
     render() {
         const { topNav } = this.props
-        return (
-            <div>
-                <button className={`btn ${styles.btn_sidenav} btn-lg page-scroll`} onClick={e => this.openNav()}>☰ open</button>
-                <div id="sideNavigation" className={styles.sidenav} ref={sidenav => {this.sidenav=sidenav}}>
-                    <button
-                        className={styles.closebtn}
-                        onClick={() => this.closeNav()}
-                    >
-                        &times;
-                    </button>
-                    {topNav.map(page => <NavButton data={page} key={page.title} />)}
-                </div>
+        return <div>
+            <button className={`btn ${styles.btn_sidenav} btn-lg page-scroll`} onClick={() => this.openNav()}>☰ open</button>
+            <div id="sideNavigation" className={styles.sidenav} ref={sidenav => {this.sidenav=sidenav}}>
+                <button
+                    className={styles.closebtn}
+                    onClick={() => this.closeNav()}
+                >
+                    &times;
+                </button>
+                {topNav.map(page =>
+                    <Link to={page.path} onClick={() => this.closeNav()}>
+                        {page.title}
+                    </Link>
+                )}
             </div>
-        )
+        </div>
     }
 }
-
-const NavButton = ({ data }) => <Link to={data.path}>{data.title}</Link>
 
 const mapStateToProps = ({ topNav }) => ({ topNav }) // Not an identity function!
 
