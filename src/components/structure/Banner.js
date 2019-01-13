1713ca036c5ec69692bf27214eb982f130b043e9
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-01-13 19:14:09 +0200

    made banner transition work

--- src/components/structure/Banner.js
+++ src/components/structure/Banner.js
@@ -1,55 +1,42 @@
 import React from 'react'
-import { connect } from "react-redux"
-import * as actions from "../../actions"
-import ImageFade from './ImageFade'
-
+import { connect } from 'react-redux'
+import styles from "./Banner.module.css"
 
 class Banner extends React.Component {
 
     constructor() {
-        super();
-        this.state = {
-            index: 0,
-            max: -1
-        };
+        super()
+        this.state = { visible: null }
     }
 
-    //This will keep resetting the banner
-    componentDidMount() {
-        this.interval = setInterval(() => this.changeCounter(), 15000);
-    }
+    componentWillReceiveProps(nextProps) {
+        const { banners } = nextProps
+        this.setState({ visible: randomElem(banners) })
 
-    //The method will randomize a new banner image which is not the same as the previous one
-    changeCounter = () => {
-        //In case there's just one banner image..
-        if (this.state.max !== 1) {
-            
-            //If the length hasn't been defined yet, async issues
-            if (this.state.max === -1) {
-                this.setState({max: this.props.banners.length - 1})
-            }
-            let number = this.state.index
-
-            while (number === this.state.index) {
-                console.log(number)
-                number = Math.floor(Math.random() * this.state.max) + 1
-            }
-            this.setState({ index: number })
-        }
+        clearInterval(this.interval)
+        this.interval = setInterval(() => {
+            const invisible = banners.filter(b => b !== this.state.visible)
+            this.setState({ visible: randomElem(invisible) })
+        }, 15000)
     }
 
     render() {
         const { banners } = this.props
-        console.log(banners)
-        console.log(banners[this.state.index])
         return (
             <div>
-                <ImageFade img={banners[this.state.index]} />
+                {banners.map(b =>
+                    <img
+                        src={b.url}
+                        key={b.url}
+                        className={b === this.state.visible ? styles.visible : styles.invisible}
+                    />
+                )}
             </div>
-
         )
     }
 }
+
+const randomElem = (array) => array[Math.floor(Math.random() * array.length)]
 
 const mapStateToProps = ({ banners }) => ({ banners }) // Not an identity function!
 
--- src/reducers/bannerReducer.js
+++ src/reducers/bannerReducer.js
@@ -1,10 +1,10 @@
-import { FETCH_BANNERS } from "../actions/types";
+import { FETCH_BANNERS } from "../actions/types"
 
-export default (state = false, action) => {
-  switch (action.type) {
-    case FETCH_BANNERS:
-      return action.payload
-    default:
-      return state;
-  }
-};
+export default (state = [], action) => {
+    switch (action.type) {
+        case FETCH_BANNERS:
+            return action.payload
+        default:
+            return state
+    }
+}
