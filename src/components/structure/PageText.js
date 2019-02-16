980067cc38ba925f33c1c5e00bbae5835e18e46b
KeremAtak <kerem.atak@hotmail.com>
2019-02-16 22:32:26 +0200

    tuning image padding

--- src/components/structure/PageText.js
+++ src/components/structure/PageText.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { connect } from "react-redux"
 import { fetchPageByPath } from "../../actions"
@@ -7,35 +8,44 @@ import { convertFromRaw } from 'draft-js'
 import styles from "./PageText.module.css"
 
 const PageText = ({ text }) =>
-    <div className={styles.pageText}>
-        <hr className={styles.hr} />
-        <div dangerouslySetInnerHTML={{ __html: textToHtml(text) }} />
-        <hr className={styles.hr} />
-    </div>
-    
+    <div className={styles.container} >
+        <div className={styles.pageText}>
+            <hr className={styles.hr} />
+            <div dangerouslySetInnerHTML={{ __html: textToHtml(text) }} />
+            <hr className={styles.hr} />
+        </div>
+    </div >
+
 const textToHtml = (text) =>
     stateToHTML(convertFromRaw(JSON.parse(text)), options)
 
 const options = {
+    // Adding css styles to emulate the look of it in css
+    // The images also have 40% displayed width on editor added. If not resized the width will be original.
+    // This will add 40% tag if this is the case.
     entityStyleFn: (entity) => {
         const entityType = entity.get('type').toLowerCase()
         if (entityType === 'image') {
             const data = entity.getData()
+
+            console.log(typeof data.width === "undefined")
             return {
                 element: 'img',
                 attributes: {
                     src: data.src,
                     align: getAlign(data.alignment),
                     className: 'img-' + data.alignment,
-                    width: data.width + '%'
+                    width: getWidth(data.width)
                 }
             }
         }
     }
 }
 
-const getAlign = alignment => alignment === 'center' ? "middle" : alignment
+// These two are kinda bubblegum, but it'll work for our purposes
+const getWidth = width => (typeof width === "undefined") ? "40%" : width + '%'
+const getAlign = alignment => alignment === 'center' || (typeof alignment === "undefined") ? "middle" : alignment
 
 const mapStateToProps = ({ page }) => ({ page }) // Not an identity function!
 
-export default connect(mapStateToProps, {fetchPageByPath})(PageText)
+export default connect(mapStateToProps, { fetchPageByPath })(PageText)
