d764590baf621e6fb514e05cf91dc8fe530273be
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-02-28 22:56:21 +0200

    refactored login to use hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import Banner from "./components/structure/Banner"
 import SideNav from "./components/structure/SideNav"
 import Footer from "./components/structure/Footer"
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import { fetchPages, fetchContact, fetchUser, fetchImages, fetchBanners, fetchAnnouncements } from "./actions"
 import { connect } from "react-redux"
 import PageNotFound from "./components/pagetypes/PageNotFound"
@@ -61,4 +61,4 @@ const renderRoutes = (pages) =>
 
 const mapStateToProps = ({ pages }) => ({ pages }) // Not an identity function!
 
-export default connect(mapStateToProps, { fetchPages, fetchContact, fetchUser, fetchImages, fetchBanners, fetchAnnouncements })(App)
+export default withRouter(connect(mapStateToProps, { fetchPages, fetchContact, fetchUser, fetchImages, fetchBanners, fetchAnnouncements })(App))
--- src/components/pagetypes/Login.js
+++ src/components/pagetypes/Login.js
@@ -1,110 +1,76 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { useState } from "react"
+import { connect } from "react-redux"
 import { firebaseAuth } from '../../config/firebase'
 
-const INITIAL_STATE = {
-    email: '',
-    password: '',
-    error: null,
-};
+const Login = ({ history, auth }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-class Login extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            ...INITIAL_STATE
-        };
-    }
-
-    static contextTypes = {
-        router: PropTypes.object
-    };
-
-    componentWillUpdate(nextProps) {
-        if (nextProps.auth) {
-            this.context.router.history.push("/");
-        }
-    }
-
-    onSubmit = event => {
-        event.preventDefault();
-        const { email, password } = this.state;
+    const onSubmit = event => {
+        event.preventDefault()
         firebaseAuth
             .signInWithEmailAndPassword(email, password)
             .then(() => {
-                this.setState({ ...INITIAL_STATE });
-                this.context.router.history.push("/");
+                setEmail("")
+                setPassword("")
+                history.push("/admin")
             })
             .catch(error => {
                 alert(error)
-            });
-        event.preventDefault();
-    };
-
-    logOut = event => {
-        firebaseAuth
-            .signOut()
-            .then(() => {
-                this.context.router.history.push("/");
             })
     }
 
-    keyPress = event => {
+    const logOut = () => {
+        firebaseAuth
+            .signOut()
+            .then(() => {
+                history.push("/")
+            })
+    }
+
+    const keyPress = event => {
         if (event.key === 'Enter') {
-            this.onSubmit(event)
+            onSubmit(event)
         }
     }
 
-    returnLoginButton() {
-        const { auth, email, password, error } = this.props
-        if (!auth) {
-            return (
-                <div>
-                    <input
-                        name="email"
-                        value={email}
-                        onChange={this.onChange}
-                        type="text"
-                        placeholder="Sähköposti"
-                        onKeyPress={this.keyPress}
-                    />
-                    <input
-                        name="password"
-                        value={password}
-                        onChange={this.onChange}
-                        type="password"
-                        placeholder="Salasana"
-                        onKeyPress={this.keyPress}
-                    />
-                    <button className="btn-primary" type="submit" onClick={this.onSubmit}>
-                        Kirjaudu sisään
-                        </button>
-                    {error && <p>{error.message}</p>}
-                </div>
-            )
-        } else {
-            return (
-                <button className="btn-primary" type="button" onClick={this.logOut}>
-                    Kirjaudu ulos tililtä {auth.email}
-            </button>
-            )
+    const onChange = event => {
+        if (event.target.name === "email") {
+            setEmail(event.target.value)
+        } else if (event.target.name === "password") {
+            setPassword(event.target.value)
         }
     }
 
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
-
-    render() {
+    if (!auth) {
         return <div>
-            {this.returnLoginButton()}
+            <input
+                name="email"
+                value={email}
+                onChange={onChange}
+                type="text"
+                placeholder="Sähköposti"
+                onKeyPress={keyPress}
+            />
+            <input
+                name="password"
+                value={password}
+                onChange={onChange}
+                type="password"
+                placeholder="Salasana"
+                onKeyPress={keyPress}
+            />
+            <button className="btn-primary" type="submit" onClick={onSubmit}>
+                Kirjaudu sisään
+            </button>
         </div>
+    } else {
+        return <button className="btn-primary" type="button" onClick={logOut}>
+                Kirjaudu ulos tililtä {auth.email}
+        </button>
     }
 }
 
 const mapStateToProps = ({ auth }) => ({ auth })
 
-export default connect(mapStateToProps)(Login);
+export default connect(mapStateToProps)(Login)
