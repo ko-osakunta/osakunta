8aed325246b2834df83c6b24074cc0e0966a5f1c
KeremAtak <kerem.atak@hotmail.com>
2019-04-18 19:42:55 +0300

    announcement dates aligned to center

--- src/components/pagetypes/Announcements.js
+++ src/components/pagetypes/Announcements.js
@@ -3,18 +3,22 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 import PageText from "../structure/PageText"
 import EditorClass from "../editor/EditorClass"
+import styles from "../structure/PageText.module.css"
+
 
 const Announcements = ({ announcements }) => <>
     {announcements.sort((a, b) => b.dateUpdated - a.dateUpdated)
         .map(({ dateCreated, dateUpdated, text, key }) =>
             <div key={key}>
-                <p>Luotu: {moment(dateCreated).format('LLL')} Päivitetty: {moment(dateUpdated).format('LLL')}</p>
+                <div className={styles.container}>
+                    <p>Luotu: {moment(dateCreated).format('LLL')} Päivitetty: {moment(dateUpdated).format('LLL')}</p>
+                </div>
                 <PageText text={text} />
                 <EditorClass text={text} updatePath={'announcements/' + key} />
             </div>
         )
     }
-</>
+    </>
 
 const mapStateToProps = ({ announcements }) => ({ announcements })
 
